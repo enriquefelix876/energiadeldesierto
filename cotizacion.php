622ad22ba542eda6cc1537d467efc5d36d4d06b6
Enrique Felix <enriquefelix876@gmail.com>
2019-02-23 22:46:08 -0700

    tonight

--- cotizacion.php
+++ cotizacion.php
@@ -1,37 +1,50 @@
 <?php 
     switch ($_POST['tarifa']) {
-        case '1E':
-            require_once('php/proyeccion1E.php');
-        break;
 
         case '1':
-            require_once('php/cotizar1.php');
+            require_once('php/proyeccion1.php');
         break;
 
         case '1A':
-        require_once('php/cotizar1A.php');
+        require_once('php/proyeccion1A.php');
         break;
 
         case '1B':
-        require_once('php/cotizar1B.php');
+        require_once('php/proyeccion1B.php');
+        break;
+
+        case '1C':
+        require_once('php/proyeccion1C.php');
+        break;
+
+        case '1D':
+        require_once('php/proyeccion1C.php');
+        break;
+
+        case '1E':
+        require_once('php/proyeccion1E.php');
+        break;
+
+        case '1F':
+        require_once('php/proyeccion1F.php');
         break;
 
         default:
         break;
     }
 
-    echo $totalDiciembre*1.16."<br>";
-    echo $totalEnero*1.16."<br>";
-    echo $totalFebrero*1.16."<br>";
-    echo $totalMarzo*1.16."<br>";
-    echo $totalAbril*1.16."<br>";
-    echo $totalMayo*1.16."<br>";
-    echo $totalJunio*1.16."<br>";
-    echo $totalJulio*1.16."<br>";
-    echo $totalAgosto*1.16."<br>";
-    echo $totalSeptiembre*1.16."<br>";
-    echo $totalOctubre*1.16."<br>";
-    echo $totalNoviembre*1.16."<br>";
+    echo $totalDiciembre."<br>";
+    echo $totalEnero."<br>";
+    echo $totalFebrero."<br>";
+    echo $totalMarzo."<br>";
+    echo $totalAbril."<br>";
+    echo $totalMayo."<br>";
+    echo $totalJunio."<br>";
+    echo $totalJulio."<br>";
+    echo $totalAgosto."<br>";
+    echo $totalSeptiembre."<br>";
+    echo $totalOctubre."<br>";
+    echo $totalNoviembre."<br><br>";
 
     echo $consumoEnero['intermedioBajo']+$consumoFebrero['intermedioBajo']+$consumoMarzo['intermedioBajo']+
     $consumoMayo['intermedioBajo']+$consumoJunio['intermedioBajo']+$consumoJulio['intermedioBajo']+
